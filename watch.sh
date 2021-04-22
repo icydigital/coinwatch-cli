c512a8ee793a621d1a8015fc0d1bcb7e450f1833
Balou9 <emmanuel.owosekun@gmail.com>
2021-04-22 20:06:34 +0200

    chore: init

---
+++ watch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source ./.env
+resp_body="$(mktemp)"
+today=$(date '+%Y-%m-%d')
+
+curl https://rest.coinapi.io/v1/exchanges \
+  --request GET \
+  --header "X-CoinAPI-Key: $X_COIN_API_KEY" | \
+  jq '.[] | "\(.name) \(.data_start)"' | grep "$today"
+>> $resp_body
