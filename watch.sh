e4acc1cbeb1f12236a8b885dcaf90f5ef9b59695
Balou9 <emmanuel.owosekun@gmail.com>
2021-04-22 20:13:23 +0200

    chore: removes flag

--- watch.sh
+++ watch.sh
@@ -4,7 +4,6 @@ resp_body="$(mktemp)"
 today=$(date '+%Y-%m-%d')
 
 curl https://rest.coinapi.io/v1/exchanges \
-  --request GET \
   --header "X-CoinAPI-Key: $X_COIN_API_KEY" | \
   jq '.[] | "\(.name) \(.data_start)"' | grep "$today"
 >> $resp_body
