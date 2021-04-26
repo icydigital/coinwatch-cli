044ae9f9b72df199295ee97c2555d7d06e1390ed
Balou9 <emmanuel.owosekun@gmail.com>
2021-04-26 08:23:05 +0200

    chore: adds coingecko

--- watch.sh
+++ watch.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 source ./.env
-resp_body="$(mktemp)"
 today=$(date '+%Y-%m-%d')
 
-coins=$(curl -s https://rest.coinapi.io/v1/exchanges \
+X_coin=$(curl -s https://rest.coinapi.io/v1/exchanges \
   --header "X-CoinAPI-Key: $X_COIN_API_KEY" | \
   jq '.[] | "\(.name) \(.data_start)"' | \
   grep "$today"
 >> /dev/null)
 
-if [ -s $coins ]
+Coingecko=$(curl -s https://coingecko.p.rapidapi.com/exchanges \
+	--header "x-rapidapi-host: coingecko.p.rapidapi.com" \
+	--header "x-rapidapi-key: $COIN_GECKO" | \
+  jq '.[]'
+>> /dev/null)
+
+echo "$X_coin"
+echo "$Coingecko"
+
+if [ -s "$X_Coin" ] && [ -s "$Coingecko"]
 then
   notify-send "No new cryptocurrencies were introduced today"
 else
-  notify-send "The following cryptocurrencies were introduced today: $(echo $coins)"
+  notify-send "The following cryptocurrencies were introduced today:
+  ===========
+  $(echo $X_Coin $Coingecko)"
 fi
