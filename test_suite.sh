622242eebccd9026caf67b3651a84d3c8a215cf0
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-05 13:47:53 +0200

    chore: observing payload

--- test_suite.sh
+++ test_suite.sh
@@ -2,5 +2,6 @@
 test_x_coin_api_get_exchanges_by_date () {
   printf "test_x_coin_api_get_exchanges_by_date\n"
   payload=(./coinwatch.sh 2015-05-17)
+  echo "$payload"
   assert_equal $payload "BTCTRADE 2015-05-17"
 }
