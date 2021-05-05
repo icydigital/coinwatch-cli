e1173ccc124416cd640fc7f30ed013985d7afbf9
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-05 22:15:21 +0200

    ci: adds quotes

--- test_suite.sh
+++ test_suite.sh
@@ -2,14 +2,12 @@
 test_get_list_of_exchanges_by_date () {
   printf "test_get_list_of_exchanges_by_date\n"
   X_COIN_API_KEY=$1
-  PRELOAD="BTCTRADE 2015-05-17"
+  PRELOAD='"BTCTRADE 2015-05-17"'
 
   PAYLOAD=$(./coinwatch.sh $X_COIN_API_KEY 2015-05-17)
-  
+
   echo "$PAYLOAD"
   echo "$PRELOAD"
-  cat "$PAYLOAD"
-  cat "$PRELOAD"
   diff -u <(echo "$PAYLOAD") <(echo "$PRELOAD")
 
   assert_equal $PAYLOAD $PRELOAD
