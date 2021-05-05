b4bf35144150a83aee79639b8ccd912a8b1c35bc
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-05 13:49:07 +0200

    chore: save command output to string

--- test_suite.sh
+++ test_suite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 test_x_coin_api_get_exchanges_by_date () {
   printf "test_x_coin_api_get_exchanges_by_date\n"
-  payload=(./coinwatch.sh 2015-05-17)
+  payload=$(./coinwatch.sh 2015-05-17)
   echo "$payload"
   assert_equal $payload "BTCTRADE 2015-05-17"
 }
