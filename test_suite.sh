669e6a171b91d4347cf1a5c02ba68fbd1feecb5b
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-05 21:55:49 +0200

    chore: renames test

--- test_suite.sh
+++ test_suite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-test_x_coin_api_get_exchanges_by_date () {
-  printf "test_x_coin_api_get_exchanges_by_date\n"
+test_get_list_of_exchanges_by_date () {
+  printf "test_get_list_of_exchanges_by_date\n"
   X_COIN_API_KEY=$1
   PAYLOAD=$(./coinwatch.sh $X_COIN_API_KEY 2015-05-17)
   echo "$PAYLOAD"
