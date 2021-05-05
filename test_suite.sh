7b7a01cfd296ccba079b0c6582bc003b4ed2d604
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-05 22:10:51 +0200

    test: works with variables

--- test_suite.sh
+++ test_suite.sh
@@ -2,7 +2,15 @@
 test_get_list_of_exchanges_by_date () {
   printf "test_get_list_of_exchanges_by_date\n"
   X_COIN_API_KEY=$1
+  PRELOAD="BTCTRADE 2015-05-17"
+
   PAYLOAD=$(./coinwatch.sh $X_COIN_API_KEY 2015-05-17)
+  
   echo "$PAYLOAD"
-  assert_equal $PAYLOAD "BTCTRADE 2015-05-17"
+  echo "$PRELOAD"
+  cat "$PAYLOAD"
+  cat "$PRELOAD"
+  diff -u <(echo "$PAYLOAD") <(echo "$PRELOAD")
+
+  assert_equal $PAYLOAD $PRELOAD
 }
