150482b9b2a8c8fa1ecfd8ecb79533ae129bc82f
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-06 22:46:00 +0200

    chore: adds response file to curl

--- coinwatch.sh
+++ coinwatch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 get_x_coin_data() {
+  resp="$(mktemp)"
   x_coin_api_key=$(echo $1 | base64 --decode)
   date=$2
 
   x_coin_payload=$(curl -s https://rest.coinapi.io/v1/exchanges \
     -X GET \
     -H "X-CoinAPI-Key: $x_coin_api_key" | \
+    -D "$resp" | \
     jq '.[] | "\(.name) \(.data_start)"' | \
     grep "$date"
   >> /dev/null)
