e2c79193ab07893ec3df6196ded66c323ef36685
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-06 12:02:31 +0200

    chore: forces error to observe larger payload

--- test_suite.sh
+++ test_suite.sh
@@ -4,9 +4,10 @@ test_get_list_of_exchanges_by_date () {
   X_COIN_API_KEY=$1
   PRELOAD=$(echo 'BTCTRADE 2015-05-17')
 
-  PAYLOAD=$(./coinwatch.sh $X_COIN_API_KEY 2015-05-17)
+  PAYLOAD=$(./coinwatch.sh $X_COIN_API_KEY 2015)
 
   echo "$PAYLOAD"
+  cat "$PAYLOAD"
   echo "$PRELOAD"
   diff -u <(echo "$PAYLOAD") <(echo "$PRELOAD")
 
