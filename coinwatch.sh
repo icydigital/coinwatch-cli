05b610ba1a1bf2905a639baf55258ba95fe44650
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-07 21:49:15 +0200

    chore: removes function call

--- coinwatch.sh
+++ coinwatch.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-get_x_coin_data() {
-  x_coin_api_key=$(echo $1 | base64 --decode)
-  date=$2
+x_coin_api_key=$(echo $1 | base64 --decode)
+date=$2
 
-  x_coin_payload=$(curl -s https://rest.coinapi.io/v1/exchanges \
-    -X GET \
-    -H "X-CoinAPI-Key: $x_coin_api_key" | \
-    jq '.[] | "\(.name) \(.data_start)"' | \
-    grep "$date"
-  >> /dev/null)
+x_coin_payload=$(curl -s https://rest.coinapi.io/v1/exchanges \
+  -X GET \
+  -H "X-CoinAPI-Key: $x_coin_api_key" | \
+  jq '.[] | "\(.name) \(.data_start)"' | \
+  grep "$date"
+>> /dev/null)
 
-  echo $x_coin_payload
-}
-
-get_x_coin_data $1 $2
+echo $x_coin_payload
