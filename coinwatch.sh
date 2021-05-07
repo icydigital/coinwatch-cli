8be8c7d244f8f233ad45d66d0f2a189e0d877033
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-07 22:46:10 +0200

    chore: observing payload to understand it since data_start is missing

--- coinwatch.sh
+++ coinwatch.sh
@@ -5,8 +5,9 @@ date=$2
 x_coin_payload=$(curl -s https://rest.coinapi.io/v1/exchanges \
   -X GET \
   -H "X-CoinAPI-Key: $x_coin_api_key" | \
-  jq '.[] | "\(.name) \(.data_start)"' | \
-  grep "$date"
+  jq '.[]'
+  # jq '.[] | "\(.name) \(.data_start)"' | \
+  # grep "$date"
 >> /dev/null)
 
 echo $x_coin_payload
