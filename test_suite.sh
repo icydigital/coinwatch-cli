c15376d468e916f7e5e4b64ed52043092273450a
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-12 16:39:11 +0200

    test: adds assertion test_coinwatch_sh

--- test_suite.sh
+++ test_suite.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 test_get_exchanges_200() {
   printf "test_get_exchanges_200\n"
-  resp_head="$(mktemp)"
 
+  resp_head="$(mktemp)"
   X_COIN_API_KEY=$1
 
   curl -sS https://rest.coinapi.io/v1/exchanges \
@@ -17,6 +17,7 @@ test_get_exchanges_200() {
 
 test_coinwatch_get_list_of_exchanges_by_date() {
   printf "test_get_list_of_exchanges_by_date\n"
+
   resp_head="$(mktemp)"
   resp_body="$(mktemp)"
   X_COIN_API_KEY=$1
@@ -40,14 +41,18 @@ test_coinwatch_get_list_of_exchanges_by_date() {
 
 test_coinwatch_sh() {
   printf "test_coinwatch_sh\n"
+
   resp_body="$(mktemp)"
   X_COIN_API_KEY=$1
   date=$2
 
-  resp_body=$(./coinwatch.sh $X_COIN_API_KEY $date)
+  ./coinwatch.sh $X_COIN_API_KEY $date >> $resp_body
   expexted_string="Token Store 2017-09-17"
 
-  if grep -xq "$expexted_string" "$resp_body"; then
-    cat "$resp_body"
+  if grep -xq "$expexted_string" "$resp_body";
+  then
+    printf "response does match expected string"
+  else
+    exit 1
   fi
 }
