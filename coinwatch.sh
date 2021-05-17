4eceb74b01ca3d34075bb16fb6872690451feeae
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-17 08:45:07 +0200

    chore: pass args down

--- coinwatch.sh
+++ coinwatch.sh
@@ -8,32 +8,37 @@ watch_coinmarketcap () {
   resp_head="$(mktemp)"
   resp_body="$(mktemp)"
 
-  curl -sS https://rest.coinapi.io/v1/exchanges \
+  curl -sS "https://rest.coinapi.io/v1/exchanges" \
     -X GET \
     --header "X-CoinAPI-Key: $X_COIN_API_KEY" \
     -D $resp_head | \
-    jq -r '.[] | "\(.name) \(.data_start)"' | \
+    # jq -r '.[] | "\(.name) \(.data_start)"' | \
     grep "$1" \
   >> $resp_body
+
+  cat $resp_body
 }
 
 watch_nomics () {
   resp_head="$(mktemp)"
   resp_body="$(mktemp)"
 
-  curl -sS https://api.nomics.com/v1/currencies/ticker \
-    -X GET \
-    --header "key: $NOMICS_API_KEY" \
+  curl -sS "https://api.nomics.com/v1/currencies/ticker?key=$NOMICS_API_KEY" \
     -D $resp_head \
   >> $resp_body
+
+  # cat $resp_body
 }
 
 get_coins () {
-  watch_coinmarketcap $date
+  resp_body="$(mktemp)"
+  watch_coinmarketcap $1 >> $resp_body
+  cat $resp_body
 }
 
 coinwatch () {
-  get_coins $date
+  resp_body="$(mktemp)"
+  get_coins $1 >> $resp_body
 }
 
 if [ -z "$notifier" ]
