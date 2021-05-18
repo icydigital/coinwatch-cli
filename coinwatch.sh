71c15e56e9a52ae5836c82c66248a8a864a45b7a
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-18 21:27:56 +0200

    chore: use resp elsewhere

--- coinwatch.sh
+++ coinwatch.sh
@@ -4,41 +4,35 @@ date=$1
 notifier=$2
 message="$(mktemp)"
 
-watch_coinmarketcap () {
-  resp_head="$(mktemp)"
-  resp_body="$(mktemp)"
+watch_cmc () {
+  resp_head_cmc="$(mktemp)"
+  resp_body_cmc="$(mktemp)"
 
   curl -sS "https://rest.coinapi.io/v1/exchanges" \
     -X GET \
     --header "X-CoinAPI-Key: $X_COIN_API_KEY" \
-    -D $resp_head | \
+    -D $resp_head_cmc \
     # jq -r '.[] | "\(.name) \(.data_start)"' | \
-    grep "$1" \
-  >> $resp_body
-
-  cat $resp_body
+    # grep "$1" \
+  >> $resp_body_cmc
 }
 
 watch_nomics () {
-  resp_head="$(mktemp)"
-  resp_body="$(mktemp)"
+  resp_head_nomics="$(mktemp)"
+  resp_body_nomics="$(mktemp)"
 
   curl -sS "https://api.nomics.com/v1/currencies/ticker?key=$NOMICS_API_KEY" \
-    -D $resp_head \
-  >> $resp_body
-
-  # cat $resp_body
+    -D $resp_head_nomics \
+  >> $resp_body_nomics
 }
 
 get_coins () {
-  resp_body="$(mktemp)"
-  watch_coinmarketcap $1 >> $resp_body
-  cat $resp_body
+  watch_cmc
+  cat $resp_body_cmc
 }
 
 coinwatch () {
-  resp_body="$(mktemp)"
-  get_coins $1 >> $resp_body
+  get_coins
 }
 
 if [ -z "$notifier" ]
