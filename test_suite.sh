bd16ca58ba1d5bdd8a43539e9261512f30b0504b
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-18 21:56:08 +0200

    test: adds test_coinwatch_get_list_of_exchanges_by_date_nomics_200

--- test_suite.sh
+++ test_suite.sh
@@ -49,6 +49,25 @@ test_coinwatch_get_list_of_exchanges_by_date_cmc_200() {
   fi
 }
 
+test_coinwatch_get_list_of_exchanges_by_date_nomics_200() {
+  printf "test_coinwatch_get_list_of_exchanges_by_date_nomics_200\n"
+
+  resp_head="$(mktemp)"
+  resp_body="$(mktemp)"
+  date=$1
+
+  curl -sS "https://api.nomics.com/v1/currencies/ticker?key=$NOMICS_API_KEY" \
+    -D $resp_head \
+  >> $resp_body
+
+  expexted_string="Token Store 2017-09-17"
+
+  assert_status $resp_head 200
+  if grep -xq "$expexted_string" "$resp_body"; then
+    cat "$resp_body"
+  fi
+}
+
 test_coinwatch_sh() {
   printf "test_coinwatch_sh\n"
 
