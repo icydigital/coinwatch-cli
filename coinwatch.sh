3c18df61b3b1ceb4220bd4442514ef8377640878
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-22 17:25:30 +0200

    chore: adds quotes

--- coinwatch.sh
+++ coinwatch.sh
@@ -20,7 +20,7 @@ watch_nomics () {
 
   curl -sS "https://api.nomics.com/v1/currencies/ticker?key=$NOMICS_API_KEY" \
     -D $resp_head_nomics | \
-    jq -r '.[] | .name + " " + ${.first_trade::-10}' \
+    jq -r '.[] | .name + " " + "${.first_trade::-10}"' \
   >> $resp_body_nomics
 }
 
