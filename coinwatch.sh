3e2e8419b3617ea0896cefb3cc33d665430238cd
Balou9 <emmanuel.owosekun@gmail.com>
2021-05-22 22:31:00 +0200

    chore: cuts start of the day timestamp

--- coinwatch.sh
+++ coinwatch.sh
@@ -20,7 +20,7 @@ watch_nomics () {
 
   curl -sS "https://api.nomics.com/v1/currencies/ticker?key=$NOMICS_API_KEY" \
     -D $resp_head_nomics | \
-    jq -r '.[] | select(.first_trade != null) | .name + " " + .first_trade' | \
+    jq -r '.[] | select(.first_trade != null) | .name + " " + .first_trade | rev | cut -c11- | rev' | \
     grep "$1" \
   >> $resp_body_nomics
 }
