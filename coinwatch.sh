508fdf836a21570035c0d916b567151a8eb40526
Balou9 <emmanuel.owosekun@gmail.com>
2021-09-20 18:32:05 +0200

    wip: removes external program args

--- coinwatch.sh
+++ coinwatch.sh
@@ -2,6 +2,7 @@
 # test -f ./.env && source ./.env
 X_COIN_API_KEY="D9CEE857-9F32-42BD-A01B-0774DFD65EB3"
 NOMICS_API_KEY="e1cb22a6e8b5b4fbe699353f2398d1ce66c9bfe2"
+date_today=$(date +"%Y-%m-%d")
 
 watch_cmc () {
   resp_head_cmc="$(mktemp)"
@@ -12,7 +13,7 @@ watch_cmc () {
     --header "X-CoinAPI-Key: $X_COIN_API_KEY" \
     -D $resp_head_cmc | \
     jq -r '.[] | .name + " " + .data_start' | \
-    grep "$1" \
+    grep "$date_today" \
   >> $resp_body_cmc
 }
 
@@ -24,7 +25,7 @@ watch_nomics () {
     -X GET \
     -D $resp_head_nomics | \
     jq -r '.[] | select(.first_trade != null) | .name + " " + .first_trade' | \
-    grep "$1" | \
+    grep "$date_today" | \
     rev | cut -c11- | rev \
   >> $resp_body_nomics
 }
@@ -37,32 +38,25 @@ watch_massari () {
     -X GET \
     -D $resp_head_massari | \
     jq -r '.data | .[].name + " " + .[].profile.economics.launch.initial_distribution.token_distribution_date' | \
-    grep "$1" | \
+    grep "$date_today" | \
     rev | cut -c11- | rev \
   >> $resp_body_massari
 }
 
 get_coins () {
   resp_body="$(mktemp)"
-  watch_cmc $1
-  watch_nomics $1
-  watch_massari $1
+  watch_cmc $date_today
+  watch_nomics $date_today
+  watch_massari $date_today
   cat "$resp_body_cmc" "$resp_body_nomics" "$resp_body_massari" | sort >> $resp_body
 }
 
 coinwatch () {
-  get_coins $1
+  get_coins $date_today
   cat "$resp_body"
 }
 
-date=$1
-notifier=$2
 message="$(mktemp)"
 
-if [ -z "$notifier" ]
-then
-  coinwatch $date
-else
-  coinwatch $date >> $message
-  notify-send "New coins on: $date" "$(cat "$message")"
-fi
+coinwatch >> $message
+notify-send "New coins on: $date" "$(cat "$message")"
