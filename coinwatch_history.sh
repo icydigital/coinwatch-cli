f1fb52ad875f7b7437641e30f495eab2645a3473
Balou9 <emmanuel.owosekun@gmail.com>
2023-08-27 23:26:43 +0200

    wip: back to it

--- coinwatch_history.sh
+++ coinwatch_history.sh
@@ -65,7 +65,7 @@ if [ -s $message ]; then
   message_header="New crypto currencies on: $date_today"
   echo $message_header && cat $message
 else
-  message_header="--- No new crypto currencies on $date_today ---"
+  message_header="--- No new crypto currencies on $date_today    ---"
   echo $message_header
 fi
 # notify-send "$(echo $message_header)" "$(cat "$message")"
